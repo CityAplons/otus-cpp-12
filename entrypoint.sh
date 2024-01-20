87cb8035111b50e4c7b66641a6ab291da2b0d03d
Nikita Mikhailovskiy <nmihaylovskiy@gmail.com>
2024-01-20 23:51:45 +0600

    Add debug mode, move artifacts to the build subdirectory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
 set -e
 
-cmake -B `pwd` -DPROJECT_REVISION=$INPUT_REVISION
-cmake --build `pwd`
-cmake --build `pwd` --target test
-cmake --build `pwd` --target package
+[ -z "$INPUT_BUILD" ] && echo "Skipping build...\n Debug mode :)" && /bin/bash && exit 0;
+
+cmake -B `pwd`/build -DPROJECT_REVISION=$INPUT_REVISION
+cmake --build `pwd`/build
+cmake --build `pwd`/build --target test
+cmake --build `pwd`/build --target package
